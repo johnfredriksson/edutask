ea14419152d4dcb8b49261c4fe06231bace46e76
John Fredriksson <93582861+johnfredriksson@users.noreply.github.com>
2023-05-21 21:39:24 +0200

    added tests for valid email

---
+++ backend/test/units/UserController/test_get_user_by_email.py
@@ -0,0 +1,26 @@
+import pytest
+import unittest.mock as mock
+
+from src.controllers.usercontroller import UserController
+
+# Fixture setup
+@pytest.fixture
+def sut(response):
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = response
+    mockedsut = UserController(mockedDAO)
+    return mockedsut
+
+# Test with valid email
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    'email, response, expected',
+    [
+        ('local-part@domain.host', [], None),
+        ('local-part@domain.host', ['local-part@domain.host'], 'local-part@domain.host'),
+        ('local-part@domain.host', ['local-part@domain.host', 'local-part-second@domain.host'], 'local-part@domain.host')
+    ]
+)
+def test_getUserByEmail_emailValid(sut, email, expected):
+    result = sut.get_user_by_email(email)
+    assert result == expected
